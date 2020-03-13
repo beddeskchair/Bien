f24262813636406dfd18a6bcffd2614e0fa8aaa6
Devneet Atherton <devneet@beddeskchair.com>
2020-03-13 17:41:22 +0000

    added reviews to the site

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,69 @@
+class ReviewsController < ApplicationController
+
+
+  def index
+    #this is our list page for our reviews
+    @number = rand(100)
+    @reviews = Review.all
+  end
+
+
+  def new
+    #this is the form for adding a new review
+    @review = Review.new
+  end
+
+
+  def create
+    #taking info from the form and adding it to the database
+    @review = Review.new(form_params)
+
+    #save this info to the database
+    @review.save
+
+    #redirect back to the home page
+    redirect_to root_path
+  end
+
+
+  def show
+    #individual review page
+    @review = Review.find(params[:id])
+  end
+
+
+def destroy
+  #find the individual review
+  @review = Review.find(params[:id])
+
+  #destroy
+  @review.destroy
+
+  #redirect to home page
+  redirect_to root_path
+end
+
+
+def edit
+  #find the individual review (to edit)
+  @review = Review.find(params[:id])
+end
+
+
+def update
+  #find the individual review (to update)
+  @review = Review.find(params[:id])
+
+  #update with the new info from the form
+  @review.update(form_params)
+
+  #redirect somewhere new
+  redirect_to review_path(@review)
+end
+
+
+def form_params
+  params.require(:review).permit(:title, :body, :score)
+end
+
+end
